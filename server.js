e93905dab93e6a574e21cb75b038c1b8ab62b983
tjnewshindisafal-maker <tjnewshindisafal@gmail.com>
2026-04-16 09:22:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ const io     = new Server(server, { cors:{ origin:'*' } });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname,'public')));
 
 const storage = multer.diskStorage({
